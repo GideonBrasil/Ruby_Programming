4249cbf519da49d860d0149c8cc09b5c38a42649
GideonBrasil <gidxx_z@yahoo.com>
2019-03-16 23:30:27 +0000

    create function for cheese and crackers

--- ex19.rb
+++ ex19.rb
@@ -1,4 +1,4 @@
-def cheese_and_creackers(cheese_count, boxes_of_crackers)
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
     puts "You have #{cheese_count} cheeses!"
     puts "You have #{boxes_of_crackers} boxes of crackers!"
     puts "Man that's enough for a party!"
@@ -6,5 +6,16 @@ def cheese_and_creackers(cheese_count, boxes_of_crackers)
 end
 
 puts "We can just give the function numbers directly:"
-cheese_and_creackers(20, 30)
+cheese_and_crackers(20, 30)
 
+puts "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+puts "we can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+puts "And we can combine the two, variables and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
