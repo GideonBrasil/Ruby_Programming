ad6a4618692b0d2b9ff574ae69c1d0935a1a75c7
GideonBrasil <gidxx_z@yahoo.com>
2019-03-17 05:36:04 +0000

    add output to array indexs

--- ex34.rb
+++ ex34.rb
@@ -5,15 +5,17 @@ puts "The animal you picked is #{bear}."
 
 animals2 = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
 
-ruby = animals2[1]
-peacock = animals2[2]
-bear = animals2[0]
-kangaroo = animals2[3]
-whale = animals2[4]
-peacock = animals2[2]
-platypus = animals2[5]
-kangaroo = animals2[3]
+first_ani = animals2[0]
+second_ani = animals2[1]
+third_ani = animals2[2]
+fourth_ani = animals2[3]
+fifth_ani  = animals2[4]
+sixth_ani = animals2[5]
 
-puts "The first (1st) animal is at 0 and is a #{bear}. The animal at 0 is the 1st animal and is a #{bear}."
 
-puts animals2
+puts "The first (1st) animal is at 0 and is a #{first_ani}. The animal at 0 is the 1st animal and is a #{first_ani}."
+puts "The second (2nd) animal is at 1 and is a #{second_ani}. The animal at 1 is the 2nd animal and is a #{second_ani}."
+puts "The first (3rd) animal is at 2 and is a #{third_ani}. The animal at 2 is the 3rd animal and is a #{third_ani}."
+puts "The first (4th) animal is at 3 and is a #{fourth_ani}. The animal at 3 is the 4th animal and is a #{fourth_ani}."
+puts "The first (5th) animal is at 4 and is a #{fifth_ani}. The animal at 4 is the 5th animal and is a #{fifth_ani}."
+puts "The first (6th) animal is at 5 and is a #{sixth_ani}. The animal at 5 is the 6th animal and is a #{sixth_ani}."
