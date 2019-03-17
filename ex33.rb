64a4fa55cc89a148bf4acf6d8f91c3ecc1c6c3bf
GideonBrasil <gidxx_z@yahoo.com>
2019-03-17 02:12:23 +0000

    Accessing Elements of Arrays

--- ex33.rb
+++ ex33.rb
@@ -1,7 +1,18 @@
-i = 0
+print "Pick a starting number for the array: "
+start_array_number = $stdin.gets.chomp
+
+print "Pick an ending number for the array: "
+end_array_number = $stdin.gets.chomp
+
 numbers = []
 
-while i < 6
+loop_array(start_array_number, end_array_number)
+
+def loop_array(start_array_number, end_array_number)
+
+i = start_array_number
+
+while i < end_array_number
     puts "At the top i is #{i}"
     numbers.push(i)
 
@@ -13,4 +24,6 @@ end
 puts "The numbers: "
 
 # remember you can write this 2 other ways?
-numbers.each {|num| puts num }
+return numbers.each {|num| puts num }
+
+end
---
+++ ex34.rb
@@ -0,0 +1,19 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+puts "The animal you picked is #{bear}."
+
+animals2 = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+ruby = animals2[1]
+peacock = animals2[2]
+bear = animals2[0]
+kangaroo = animals2[3]
+whale = animals2[4]
+peacock = animals2[2]
+platypus = animals2[5]
+kangaroo = animals2[3]
+
+puts "The first (1st) animal is at 0 and is a #{bear}. The animal at 0 is the 1st animal and is a #{bear}."
+
+puts animals2
