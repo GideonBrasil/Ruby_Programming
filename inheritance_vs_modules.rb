8c5bbf12fd8db5630375fc682be4c0b8f1f58433
GideonBrasil <gidxx_z@yahoo.com>
2019-03-18 13:19:59 +0000

    modules and method lookup path

---
+++ inheritance_vs_modules.rb
@@ -0,0 +1,5 @@
+# You can only subclass from one class. But you can mix in as many modules as you'd like.
+
+# If it's an "is-a" relationship, choose class inheritance. If it's a "has-a" relationship, choose modules. Example: a dog "is an" animal; a dog "has an" ability to swim.
+
+# You cannot instantiate modules (i.e., no object can be created from a module). Modules are used only for namespacing and grouping common methods together.
