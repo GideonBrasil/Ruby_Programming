1b7e28d3076a3d8da47ee37548af07d28f7ec9eb
GideonBrasil <gidxx_z@yahoo.com>
2019-03-18 14:02:33 +0000

    private and protected class exercises

---
+++ inheritance_ex2.rb
@@ -0,0 +1,23 @@
+class Student
+    def initialize(name, grade)
+        @name = name
+        @grade = grade
+    end
+  
+    def better_grade_than?(other_student)
+      grade > other_student.grade
+    end
+  
+    protected
+  
+    def grade
+        @grade
+    end
+end
+  
+joe = Student.new("Joe", 90)
+bob = Student.new("Bob", 84)
+puts "Well done!" if joe.better_grade_than?(bob)
+
+bob = Person.new
+bob.hi # hi is a private method so it can't be called
